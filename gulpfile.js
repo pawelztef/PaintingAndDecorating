ba42b1d330d2cce3de00da81fe84441e1ee5b2e9
Pavel <pawelztef@gmail.com>
2019-05-12 19:05:49 +0100

    GULP WATCH

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.sass')
-  .pipe(sass())
-  .pipe(gulp.dest('app/css'))
+  return gulp.src('app/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/sass/**/*.sass', gulp.series('sass'));
+  gulp.watch('app/sass/**/*.sass', gulp.series('browserSync'));
+});
+
+
