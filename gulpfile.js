26800d5568a23965c0851e9de74af31f78952585
Pavel <pawelztef@gmail.com>
2019-05-12 16:57:33 +0100

    GULP SASS Task for gulp created.

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.sass')
+  .pipe(sass())
+  .pipe(gulp.dest('app/css'))
+});
