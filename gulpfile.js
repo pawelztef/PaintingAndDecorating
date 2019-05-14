b67f6279c0714bedd433c545ae1e0b71abdbbf58
Pavel <pawelztef@gmail.com>
2019-05-14 12:01:18 +0100

    GULP REPLACE PLUGIN Setup for replacing css url paths.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var replace = require('gulp-replace');
 
 
 function styles(){
   return gulp.src('app/sass/**/*.sass')
+    .pipe(replace('url("', 'url("app/minified/images/'))
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.stream());
+}
+
+function imgSquash() {
+  return gulp.src("app/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("app/minified/images"));
 }
 
 function watch() {
@@ -17,12 +26,14 @@ function watch() {
     },
   });
 
+  gulp.watch('app/images/*', imgSquash);
   gulp.watch('app/sass/**/*.sass', styles);
   gulp.watch('app/*.html').on('change', browserSync.reload);
   gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.styles = styles;
+exports.imgSquash = imgSquash;
 exports.watch = watch;
 
 
