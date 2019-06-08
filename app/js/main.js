9b5c4804796e508610eecfbee95217539d3aa93a
Pavel <pawelztef@gmail.com>
2019-06-08 09:42:02 +0100

    GALLERY

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     window.onscroll = function() { toTop() };
     function toTop() {
-      if(document.body.scrollTop>926 || document.documentElement.scrollTop>926) {
+      if(document.body.scrollTop>816 || document.documentElement.scrollTop>816) {
         document.getElementById("totop-link").classList.add("totop-visible");
       } else {
         document.getElementById("totop-link").classList.remove("totop-visible");
