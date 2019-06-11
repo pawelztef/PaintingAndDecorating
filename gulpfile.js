86edcc857e9755ab01754a758b5758a96fb96f3f
Pavel <pawelztef@gmail.com>
2019-06-11 13:21:02 +0100

    QUICK FIX

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,54 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var imagemin = require('gulp-imagemin');
 var replace = require('gulp-replace');
+var inject = require('gulp-inject-string');
+
 var paths = {
   src: 'src/**/*',
   srcHTML: 'src/**/*.html',
-  srcSASS: 'src/**/*.sass',
-  srcJS: 'src/**/*.js',
+  srcStyles: 'src/**/*.sass',
+  srcScripts: 'src/**/*.js',
+
   tmp: 'tmp',
   tmpHTML: 'tmp/index.html',
   tmpCSS: 'tmp/**/*.css',
-  tmpJS: 'tmp/**/*.js',
+  tmpScripts: 'tmp/**/*.js',
+
   dist: 'dist',
   distHTML: 'dist/index.html',
   distCSS: 'dist/**/*.css',
-  distJS: 'dist/**/*.js'
+  distScripts: 'dist/**/*.js'
 };
 
 function html(){
   return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
 }
 
-function css() {
-  return gulp.src(paths.srcSASS)
+function styles() {
+  return gulp.src(paths.srcStyles)
     .pipe(sass())
     .pipe(gulp.dest(paths.tmp));
-    // .pipe(browserSync.stream());
+  // .pipe(browserSync.stream());
 }
 
-function javaScripts() {
-  return gulp.src(paths.srcJS)
+function scripts() {
+  return gulp.src(paths.srcScripts)
     .pipe(gulp.dest(paths.tmp));
-    // .pipe(browserSync.stream());
+  // .pipe(browserSync.stream());
 }
-function imgSquash() {
-  return gulp.src("app/images/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("app/minified/images"));
+
+function inject() {
+  return gulp.src(paths.srcStyles)
+    .pipe(sass())
+    .pipe(gulp.dest(paths.tmp));
+  // .pipe(browserSync.stream());
 }
+  // var targetTagJs = "<!-- inject:js -->";
+  // var targetTagCss = "<!-- inject:css -->";
+
+  // return gulp.src(paths.tmpHtml)
+  // .pipe(inject.after(targetTagCss, "hello"))
+  // .pipe(gulp.dest(paths.tmpCSS));
 
 function watch() {
   browserSync.init({
@@ -46,16 +58,19 @@ function watch() {
       baseDir: 'app'
     },
   });
-
   gulp.watch('app/images/*', imgSquash);
   gulp.watch('app/sass/**/*.sass', styles);
   gulp.watch('app/*.html').on('change', browserSync.reload);
   gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 }
 
-// exports.styles = styles;
-// exports.imgSquash = imgSquash;
+
+// const staging = gulp.series(gulp.parallel(html, styles, scripts), inject);
 // exports.watch = watch;
+// exports.staging = staging;
 exports.html = html;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.inject = inject;
 
 
